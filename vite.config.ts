389b6c80080084a82d4b666cec28b5748e1c67c1
CodeWithThree <84151954+Cheung-KaWai@users.noreply.github.com>
2024-09-28 11:28:08 +0200

    :wrench: fix imports not found using ts paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import viteReact from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import viteTsConfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), TanStackRouterVite(), viteReact()],
+  plugins: [
+    react(),
+    TanStackRouterVite(),
+    viteTsConfigPaths({
+      projects: ["./tsconfig.json"],
+    }),
+  ],
 });
